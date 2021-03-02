b2b53bac541ac13d763ca0ea4a3caff23dab0897
Clark Taylor <clark@upserv.io>
2021-03-02 13:50:23 -0600

    WIP

--- controllers/actions/show_router.rb
+++ controllers/actions/show_router.rb
@@ -1,7 +1,8 @@
   def show
     @cgn = cgn::cgn.find(params[:id])
     authorize! :cgn, @cgn
-    #-- Redirect to First Authorized Tab With Model Based Auth --#
+    
+    # Redirect to first authorized
     if can? :cgn, @cgn
       redirect_to cgn_url(@cgn)
     elsif can? :cgn, @cgn
