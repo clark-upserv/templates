9661311e3f36b73c8fa33cd7a724a47cf3ba9aa4
Clark Taylor <clark@upserv.io>
2021-03-07 21:40:10 -0600

    WIP

---
+++ helpers/options_for_select/base_options.rb
@@ -0,0 +1,16 @@
+    def Change_helper_Change_options(variable)
+      # Create opitons array programatically
+      variable.things.sort_or_order.map { |thing| [thing.display, thing.value] }
+
+      # Or
+      
+      # Create opitons array manually
+      [
+        # Display and Option will be the same
+        'Display and value',
+        # Unique display and value with array
+        ['display', 'value'],
+        # Unique display and value with hash
+        { 'display': 'value' }
+      ]
+    end
---
+++ helpers/options_for_select/grouped_options.rb
@@ -0,0 +1,39 @@
+    def Change_helper_Change_grouped_options(variable)
+      # Create opitons array programatically
+      # group the things
+      hash_of_grouped_things = variable.things.group_by { |thing| thing.group }
+      # create nested array of grouped things for select
+      array_of_grouped_options_for_select = hash_of_grouped_things.map { |group, things| [group, things.sort.map { |thing| [thing.display, thing.value] }] }
+      # sort final array 
+      sorted_array_grouped_options_for_select = array_of_grouped_options_for_select.sort
+
+      # Or
+      
+      # Create opitons array manually
+      [
+        # grouped options
+        [
+          'Group 1',
+          [
+            # Display and Option will be the same
+            'Display and value',
+            # Unique display and value with array
+            ['display', 'value'],
+            # Unique display and value with hash
+            { 'display': 'value' }
+          ]
+        ],
+        [
+          'Group 2',
+          [
+            # Display and Option will be the same
+            'Display and value',
+            # Unique display and value with array
+            ['display', 'value'],
+            # Unique display and value with hash
+            { 'display': 'value' }
+          ]
+        ]
+      ]
+    end
+
