3a2ee1e36bf983529fffde310be28bfe96833702
Clark Taylor <clark@upserv.io>
2021-03-12 11:13:15 -0600

    wip

--- controllers/strong_params.rb
+++ controllers/strong_params.rb
@@ -1,25 +1,25 @@
 # basic params
-params.require('ChangeThisPls').permit(:ChangeThisPls)
+params.require('user').permit(:name)
 
 # params for when multiple unrelated objects are submitted
-params.require('ChangeThisPls').require('ChangeThisPls').permit(:ChangeThisPls)
-params.require('ChangeThisPls').require('ChangeThisPls').permit(:ChangeThisPls)
+params.require('application').require('user').permit(:name)
+params.require('application').require('school').permit(:mascot)
 
 # params for simple array
 #   - empty array is needed to note that object is an array
-#   - [:ChangeThisPls] at end makes strong params an array of permitted params
+#   - [:ids] at end makes strong params an array of permitted params
 #     which is exactly what we want
-params.require('ChangeThisPls').permit(ChangeThisPls: [])[:ChangeThisPls]
+params.require('user').permit(ids: [])[:ids]
 
 # params for array of hashes
 #   - array with symbols is needed to note what is permitted for each
 #     hash instance that is submitted
-#   - [:ChangeThisPls] at end makes strong params an array of strong params
+#   - [:users_list] at end makes strong params an array of strong params
 #     which is exactly what we want
-params.require('ChangeThisPls').permit(ChangeThisPls: [:ChangeThisPls])[:ChangeThisPls]
+params.require('users').permit(users_list: [:name, :email])[:users_list]
 
 # accepts nested attributes for single child
-params.require('user').permit(home_attributes: [:ChangeThisPls])
+params.require('user').permit(home_attributes: [:color])
 
 # accepts nested attributes for multiple children
-params.require('user').permit(friends_attributes: [:ChangeThisPls])
+params.require('user').permit(friends_attributes: [:name, :is_cool])
