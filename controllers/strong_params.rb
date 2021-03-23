16ad4daa6e7a0b93e855357ef916af097ed89b7f
Clark Taylor <clark@upserv.io>
2021-03-23 18:49:30 -0500

    wip

--- controllers/strong_params.rb
+++ controllers/strong_params.rb
@@ -1,7 +1,8 @@
 # General Guidelines:
 # - have one param per action or form submission
+# - make sure to account for all params - DO NOT have any unpermitted params
 # - it can have multople objects and however much complexity you want
-# - then use [:some_item] to get what you want
+# - then use [:some_item] in controller to get what you want from the params
 # - be carefule for chains and nil - ex:
 #   - create_params[:user][:name] will error if create_params[:user] is nil
 
@@ -15,7 +16,7 @@ end
 #   syntax error
 # - use hash with value of empty array for param that can be an array
 # - use hash with value of filled array for single child model (array simple)
-# - use hash with value of array with hash with value of filled array multiple child models (array hash)
+# - use hash with value of array with hash with value of filled array for multiple child models (array hash)
 def create_params
   params.require('user').permit(:name,
                                 favorite_colors: [],
@@ -24,13 +25,15 @@ def create_params
 end
 
 # params where require could be nil
+# - if using submit button, make sure to include :commit so that there are not any unpermitted params
 def create_params
-  params.permit(user: [:name])
+  params.permit(:commit, user: [:name])
 end
 
 # params for array simple (because user could be nil if no favorite_colors are selected
+# - if using submit button, make sure to include :commit so that there are not any unpermitted params
 def create_params
-  params.permit(user: [favorite_colors: []])
+  params.permit(:commit, user: [favorite_colors: []])
 end
 
 # params for array hash
@@ -39,7 +42,11 @@ def create_params
 end
 
 # accepts nested attributes for single child
-params.require('user').permit(home_attributes: [:color])
+def create_params
+  params.require('user').permit(home_attributes: [:color])
+end
 
 # accepts nested attributes for multiple children
-params.require('user').permit(friends_attributes: %i[name is_cool])
+def create_params
+  params.require('user').permit(friends_attributes: %i[name is_cool])
+end
