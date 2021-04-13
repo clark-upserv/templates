5c22b338afebf0d61fd2f3f2713f9c70a1a60d0e
Clark Taylor <clark@upserv.io>
2021-04-13 15:07:09 -0500

    wip

---
+++ views/styles/color_scheme.rb
@@ -0,0 +1,24 @@
+'
+whites and grays (lighest to darkest)
+  background colors
+    white / alternating row / lightest backgrond: #FFFFFF
+    page body / alternating row gray: #F5F5F5
+    header color / darkest background: #EEEEEE
+  additional grays
+    another gray that is used for borders and some background colors: #e3e3e3;
+    border color for inputs: #d5d8de
+  text colors
+    placeholder gray: #878787
+    these three are very similar / almost the same
+      page header gray: #6b6b6b
+      "dimgray" (found in side nav): #696969
+      label gray (.text-label): #666666
+    black text: #404040
+
+main colors
+  primary: #4285f4
+  secondary:
+  warning
+  warning 2
+  danger
+'
