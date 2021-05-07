f754cab8ff2069f00d431a0c05a4e6a0ee2821a8
Clark Taylor <clark@upserv.io>
2021-05-06 19:31:52 -0500

    wip

--- controllers/actions/create_base.rb
+++ controllers/actions/create_base.rb
@@ -5,21 +5,22 @@
       @ChangeLoad.assign_attributes(create_params)
       # DeleteThis - model based auth
       authorize!(:ChangeNamespace_ChangePermission, @ChangeModel)
-      # DeleteThis - if html response
       if @ChangeLoad.save
+
+        # DeleteThis - if html response
         flash[:success] = 'ChangeObject created.'
         redirect_to ChangeUrlHelper_url
       else
         flash[:danger] = 'Failed to create ChangeObject. See errors below.'
         # DeleteThis - insert instance variables for template
         render 'ChangeTemplate'
-      end
+
       # DeleteThis - if js response
-      if @ChangeLoad.save
         flash.now[:success] = 'ChangeObject created.'
         render 'create'
       else
         flash[:danger] = 'Failed to create ChangeObject. See errors below.'
         render 'invalid_create'
+
       end
     end
--- controllers/actions/update_base.rb
+++ controllers/actions/update_base.rb
@@ -6,21 +6,22 @@
       # DeleteThis - on update, assign update params AFTER authorizing otherwise user might be able to update model they should not have access to
       @ChangeLoad.assign_attributes(update_params)
       # DeleteThis - Note: authorization is not called after assignemnt... so make sure assignment is restricted with strong params and / or attribute value restrictions and / or adding additional authorization after assignment
-      # DeleteThis - if html response
       if @ChangeLoad.save
+
+      # DeleteThis - if html response
         flash[:success] = 'ChangeObject updated.'
         redirect_to ChangeUrlHelper_url
       else
         flash[:danger] = 'Failed to update ChangeObject. See errors below.'
         # DeleteThis - insert instance variables for template
         render 'ChangeTemplate'
-      end
+
       # DeleteThis - if js response
-      if @ChangeLoad.save
         flash.now[:success] = 'ChangeObject updated.'
         render 'update'
       else
         flash[:danger] = 'Failed to update ChangeObject. See errors below.'
         render 'invalid_update'
+
       end
     end
